b941d065d47b7f421ecf34b6275470ab8636ecac
Accountant77 <vbonilla.77@gmail.com>
2020-12-06 19:38:18 +0100

    mostrar carta

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
-//BOTÓN PLAY, ABRE VENTANA DE JUEGO
-//function openGame(){
-    //document.querySelector("#play").style.display="block"
-   
-//}
-//document.getElementById("openPlay").onclick=openGame
-
-
+//BOTÓN INSTRUCCIONES, ABRE INSTRUCCIONES
+function openInstruction(){
+    document.querySelector("#instructions").style.display="flex"
+}
+document.getElementById("openInstructions").onclick=openInstruction
 //BOTÓN PLAY, ABRE VENTANA DE 321 Y JUEGO
 function openCount(){
     document.querySelector(".count_screen").style.display="flex"
@@ -15,104 +12,62 @@ function openCount(){
           clearInterval(downloadTimer);
           document.querySelector("#play").style.display="block";
           document.querySelector(".count_screen").style.display="none";
-          
         } else {
           document.getElementById("countdown2").innerHTML = timeleft;
         }
         timeleft -= 1;
       }, 1000);
-
-   
 }
 document.getElementById("openPlay").onclick=openCount
-
-
-
-//BOTÓN INSTRUCCIONES, ABRE INSTRUCCIONES
-function openInstruction(){
-    document.querySelector("#instructions").style.display="flex"
+//BOTÓN HOME, VUELVE A HOME
+function openHome(){
+    document.querySelector(".play_screen").style.display="none"
 }
-
-document.getElementById("openInstructions").onclick=openInstruction
-
+document.querySelector(".home").onclick=openHome
 //BOTÓN VOLVER, VUELVE A PANTALLA PRINCIPAL DESDE INTRUCCIONES
 function return_principlaScreen(){
     document.querySelector("#instructions").style.display="none"
 }
 document.querySelector("#return").onclick=return_principlaScreen
 
-/*
-//BOTÓN MENU, VUELVE A PANTALLA PRINCIPAL DESDE JUEGO
-function Menu_principlaScreen(){
-    document.querySelector("play_screen").style.display="none"
-}
-document.querySelector("#return2").onclick=Menu_principlaScreen
-*/
-
-
-
+//Juego
 let jugador=document.querySelector("input[name='jugador']")
 let baraja=[]
-let botonIniciar=document.querySelector("#lnkIniciar")
-let cartaCentral=document.querySelector(".cartaCentral")
+let elementos = ["raton","sillon","fantasma","botella","libro"]
+let cartaCentral = document.querySelector(".cartaCentral")
 
-for(let i=0;i<40;i++){
+for(let i=1;i<41;i++){
 let carta={
     nombre:"c"+i,
-    imagen: "img"+i,
+    imagen: "c"+i+".svg",
     id: i,
-    
-
-
 }
-baraja.push(carta)
+    baraja.push(carta)
 }
-
-
-baraja=baraja.sort(()=>Math.random()-0.5)
+baraja = baraja.sort(()=>Math.random()-0.5)
 console.log(baraja)
 
-
-//muestra primera carta    
-botonIniciar.onclick=()=>{
-
-    document.querySelectorAll(".e").forEach(el=>{
-        el.onclick=match
-    })
-    
-    cartaCentral.textContent=baraja[0].nombre
-
-} 
-
-function match(event){
-
-    console.log(event)
-    
-}
-
+let jugada = document.createElement("img")
+jugada.src = "./images/baraja/" + baraja[0].imagen
+cartaCentral.appendChild(jugada)
 
 
 //guardar jugador en local storage
-
 function guardarPartida(){
     let jugadasDe=JSON.parse(localStorage.getItem(jugador.value))
     if(jugadasDe==null) jugadasDe=[]
     jugadasDe.push(jugada)
     localStorage.setItem(jugador.value,JSON.stringify(jugadasDe))
 }
-
-
-//CUENTA ATRÁS
+//CUENTA ATRÁS PARTIDA
 var seconds = 185;
       function secondPassed() {
       var minutes = Math.round((seconds - 30)/60);
-      
       var remainingSeconds = seconds % 60;
       if (remainingSeconds < 10) {
          remainingSeconds = "0" + remainingSeconds; 
       }
       document.getElementById('countdown').innerHTML = minutes + ":" + remainingSeconds;
-      
       if (seconds == 0) {
        clearInterval(countdownTimer);
        document.getElementById('countdown').innerHTML = "¡Se acabó tu tiempo!";
@@ -121,9 +76,6 @@ var seconds = 185;
       }
       }
       var countdownTimer = setInterval('secondPassed()', 1000);
-
-
-      //CUENTA ATRÁS 321
       
 
     
